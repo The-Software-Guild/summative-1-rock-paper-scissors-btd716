35257cc22aae262697cd04117d72e6adb6acae16
btd716 <btd716@gmail.com>
2020-10-13 12:49:53 -0400

    complete

--- Assessment1/src/main/java/com/mthree/summative/dogGenetics/DogGenetics.java
+++ Assessment1/src/main/java/com/mthree/summative/dogGenetics/DogGenetics.java
@@ -15,44 +15,37 @@ public class DogGenetics {
         String dName3 = "Australian Cattle Dog";
         String dName4 = "Sheep Dog";
         String dName5 = "Papillon";
-        double breedPercent1 = 0;
-        double breedPercent2 = 0;
-        double breedPercent3 = 0;
-        double breedPercent4 = 0;
-        double breedPercent5 = 0;
-        double breedSum;
+        int breedPercent1 = 0;
+        int breedPercent2 = 0;
+        int breedPercent3 = 0;
+        int breedPercent4 = 0;
+        int breedPercent5 = 0;
+        int breedSum;
         
         System.out.println("Enter name of your dog: ");
             String dogName = usrDog.nextLine();
         System.out.println("Well then, I have this highly reliable report on " + 
                 dogName + "'s prestigious background right here: ");
-        
-        
-        
 
-        //for (int i = 0; i < 1; i++) {}
-            
+        //for (int i = 0; i < 1; i++) {}          
         do
         {
-            breedPercent1 = dogBreed.nextDouble() * 100;
-            breedPercent2 = dogBreed.nextDouble() * 100;
-            breedPercent3 = dogBreed.nextDouble() * 100;
-            breedPercent4 = dogBreed.nextDouble() * 100;
-            breedPercent5 = dogBreed.nextDouble() * 100;
+            breedPercent1 = dogBreed.nextInt(100);
+            breedPercent2 = dogBreed.nextInt(100);
+            breedPercent3 = dogBreed.nextInt(100);
+            breedPercent4 = dogBreed.nextInt(100);
+            breedPercent5 = dogBreed.nextInt(100);
             breedSum = (breedPercent1 + breedPercent2 + breedPercent3 + breedPercent4 + breedPercent5);
-            break;
-        } while (breedSum != 100);
            
-            
-           
+        } while (breedSum != 100);    
         
         System.out.println(dogName + " is: \n");
-        System.out.println(Math.round(breedPercent1) + "% " + dName1);
-        System.out.println(Math.round(breedPercent2) + "% " + dName2);
-        System.out.println(Math.round(breedPercent3) + "% " + dName3);
-        System.out.println(Math.round(breedPercent4) + "% " + dName4);
-        System.out.println(Math.round(breedPercent5) + "% " + dName5);
-        
+        System.out.println((breedPercent1) + "% " + dName1);
+        System.out.println((breedPercent2) + "% " + dName2);
+        System.out.println((breedPercent3) + "% " + dName3);
+        System.out.println((breedPercent4) + "% " + dName4);
+        System.out.println((breedPercent5) + "% " + dName5);
+        System.out.println("Total: " + breedSum);
         System.out.println("Wow! That's quite the dog you have there!");
     }
 }
